5bc09f29d207c45855905dc202c96fa1d41cb2d6
ChoiHs <chs991209@gmail.com>
2026-05-26 23:45:38 +0900

    [INIT] Android app Google Map Setting

---
+++ app/src/main/java/com/example/googlemapapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.googlemapapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+// ***********************************************
+// OnMapReadyCallback => Use Additional Class for Callback
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val seoul = LatLng(37.5665, 126.9780)
+        googleMap.addMarker(MarkerOptions().position(seoul).title("Seoul"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 12f))
+    }
+}
